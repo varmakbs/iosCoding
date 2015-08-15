8eee9b2f13048d3bf934e8a7326f2fc60d6852c9
Varma Kammili <kbsvarma@gmail.com>
2015-08-15 18:52:03 -0500

    First Commit

---
+++ Testing2/Testing2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing2
+//
+//  Created by Varma Kammili on 8/15/15.
+//  Copyright © 2015 Varma Kammili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
